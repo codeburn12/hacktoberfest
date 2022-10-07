e070e7e8a8b73c08e52aff38528ad9254268581e
Ankit Kumar <101239757+codeburn12@users.noreply.github.com>
2022-10-07 15:52:03 +0530

    helloWorld.cpp

---
+++ scripts/helloWorld.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: Ankit Kumar
+// GITHUB: https://github.com/codeburn12
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
